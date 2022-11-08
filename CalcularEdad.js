f78495dc8a0449828b764f6e07c9e89dcc63a93b
Jose Barraza <jose.barraza@akimad.com>
2022-11-07 19:55:42 -0500

    Se agrego función de multiplicar

--- CalcularEdad.js
+++ CalcularEdad.js
@@ -1,10 +1,14 @@
 function calcularEdad(edad) {
-    console.log(typeof edad)
-    if(edad >=18)
-      return "Es Mayor de edad ";
-    else 
-      return "Es menor de edad";
+  console.log(typeof edad);
+  if (edad >= 18) return "Es Mayor de edad ";
+  else return "Es menor de edad";
+}
 
+function tablaMultiplicar(numero) {
+  for (let i = 1; i <= 10; i++) {
+    console.log(numero + " x " + i + " = " + numero * i);
   }
-  
-  console.log (calcularEdad(14))
+}
+
+console.log(calcularEdad(14));
+tablaMultiplicar(3);
