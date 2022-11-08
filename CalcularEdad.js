f8adcf8193b67eb1a6b8080346521d649010c2ca
dinagutierrezc <dinagutierreztm@gmail.com>
2022-11-07 19:37:13 -0500

    calcular edad

---
+++ CalcularEdad.js
@@ -0,0 +1,10 @@
+function calcularEdad(edad) {
+    console.log(typeof edad)
+    if(edad >=18)
+      return "Es Mayor de edad ";
+    else 
+      return "Es menor de edad";
+
+  }
+  
+  console.log (calcularEdad(14))
